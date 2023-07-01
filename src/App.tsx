4ad5cd17eb16c328a6dd65f0871dff3710d7e61c
karolhas <karolekhas@gmail.com>
2023-07-01 09:14:52 +0200

    Update v1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import { BsLinkedin, BsGithub } from "react-icons/bs";
-import { BiLogoGmail, BiSolidDownload, BiSolidPrinter } from "react-icons/bi";
+import {
+  BiLogoGmail,
+  BiSolidDownload,
+  BiSolidPrinter,
+  BiLogoGit,
+} from "react-icons/bi";
 
 import { AiFillHtml5 } from "react-icons/ai";
-import { BiLogoCss3, BiLogoJavascript, BiLogoReact } from "react-icons/bi";
+import {
+  BiLogoCss3,
+  BiLogoJavascript,
+  BiLogoReact,
+  BiLogoTailwindCss,
+  BiLogoGithub,
+  BiLogoBootstrap,
+} from "react-icons/bi";
 
 import Image from "./assets/hsk.png";
 
 function App() {
   return (
     <>
-      <div className="container min-h-[200vh] bg-slate-400">
+      <div className="container min-h-[200vh] bg-gradient-to-r from-slate-300 via-slate-400 to-slate-300">
         <section className="p-6 text-center">
           <h1 className="pb-4 text-4xl font-bold">Karol Has</h1>
           <img src={Image} alt="avatar" className="mx-auto w-60 rounded-full" />
@@ -66,7 +78,7 @@ function App() {
 
         <section className="p-6">
           <h2 className="text-center text-2xl font-semibold">About</h2>
-          <p className="pt-4 text-justify text-lg font-light">
+          <p className="pt-6 text-justify text-lg font-light">
             I am a soldier trying to change my job to what I enjoy. I create
             websites with passion and simplicity. I regularly devote my free
             time to learning new things. I enjoy learning and expanding my
@@ -75,12 +87,73 @@ function App() {
           </p>
         </section>
 
-        <section>
-          <h2 className="text-center text-2xl font-semibold">Technologies</h2>
-          <AiFillHtml5 className="text-6xl text-orange-700" />
-          <BiLogoCss3 className="text-6xl text-blue-700" />
-          <BiLogoJavascript className="text-6xl text-yellow-400" />
-          <BiLogoReact className="text-6xl text-blue-500" />
+        <section className="p-6 text-center">
+          <h2 className="text-2xl font-semibold">Technologies</h2>
+          <div className="grid grid-flow-col grid-rows-2 justify-around gap-y-6 pt-6">
+            <div>
+              <AiFillHtml5 className="text-6xl text-orange-700" />
+              <p className="font-light">HTML5</p>{" "}
+            </div>
+            <div>
+              <BiLogoGit className="text-6xl text-orange-700" />
+              <p className="font-light">Git</p>{" "}
+            </div>
+
+            <div>
+              <BiLogoCss3 className="text-6xl text-blue-700" />
+              <p className="font-light">CSS3</p>{" "}
+            </div>
+            <div>
+              <BiLogoGithub className="text-6xl text-gray-900" />
+              <p className="font-light">Github</p>{" "}
+            </div>
+            <div>
+              <BiLogoJavascript className="mx-auto text-6xl text-yellow-400 " />
+              <p className="font-light">Javascript</p>{" "}
+            </div>
+            <div>
+              <BiLogoTailwindCss className="mx-auto text-6xl text-blue-400 " />
+              <p className="font-light">TailwindCSS</p>{" "}
+            </div>
+            <div>
+              <BiLogoReact className="text-6xl text-blue-500" />
+              <p className="font-light">React</p>{" "}
+            </div>
+            <div>
+              <BiLogoBootstrap className="mx-auto text-6xl text-purple-700" />
+              <p className="font-light">Bootstrap</p>{" "}
+            </div>
+          </div>
+        </section>
+
+        <section className="p-6">
+          <h2 className="text-center text-2xl font-semibold">Experience</h2>
+          <h3 className="pt-6 text-xl">Back office consultant</h3>
+          <p className="text-sm font-light text-gray-600">
+            PAYPAL | APRIL 2021 - SEPTEMBER 2021
+          </p>
+          <ul className="ml-2 list-inside list-disc pt-4 font-light">
+            <li>
+              Customer indentification with the account by the documents
+              provided by him
+            </li>
+            <li>
+              Controlling the security of international customers' transactions
+              and fighting signs of illegal activity
+            </li>
+            <li>
+              Changing the personal data of customers after sending the relevant
+              documents
+            </li>
+          </ul>
+          <div className="pt-4 text-xs">
+            <span className="rounded bg-gradient-to-b from-cyan-400 to-cyan-600 px-1 py-1">
+              English B2
+            </span>
+            <span className="ml-2 rounded bg-gradient-to-b from-cyan-400 to-cyan-600 px-1 py-1">
+              Solving problems
+            </span>
+          </div>
         </section>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,6 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    // colors: {
-    //   htmlLogo: "#e34c26",
-    //   cssLogo: "#264de4",
-    //   jsLogo: "#f0db4f",
-    // },
   },
   plugins: [],
 };

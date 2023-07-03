90d3169150591350c5270a1d1092fa1f0208ee35
karolhas <karolekhas@gmail.com>
2023-07-04 01:08:03 +0200

    Changes file path. Back to routes.

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,30 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
 
-import App from "./App";
+import App from "./App.tsx";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,
-//   },
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
-// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-//   <React.StrictMode>
-//     <RouterProvider router={router} />
-//   </React.StrictMode>
-// );
-
-const container = document.getElementById("root");
-const root = createRoot(container as HTMLDivElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
+
+// const container = document.getElementById("root");
+// const root = createRoot(container as HTMLDivElement);
+
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );

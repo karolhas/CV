166b0870575b669c8b94aa27d1c3e68cc38b6c4c
karolhas <karolekhas@gmail.com>
2023-07-11 17:10:00 +0200

    Added Footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import About from "./components/About";
 import Experience from "./components/Experience";
 import Education from "./components/Education";
 import Skills from "./components/Skills";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Skills />
       <Experience />
       <Education />
+      <Footer />
     </>
   );
 }

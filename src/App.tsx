ed727651416f5db2bfbfdda9ad4d41cc2100f7ec
karolhas <karolekhas@gmail.com>
2023-07-11 17:54:02 +0200

    Added Grid to desktop version

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,32 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
-      <Photo />
-      <Contact />
-      <Buttons />
-      <About />
-      <Skills />
-      <Experience />
-      <Education />
-      <Footer />
+      <div className="lg:grid-rows-7 lg:grid lg:max-h-screen lg:grid-cols-5">
+        <div className="lg:row-span-2">
+          <Photo />
+        </div>
+        <div className="lg:col-start-1 lg:row-span-2 lg:row-start-3">
+          <Contact />
+        </div>
+        <div className="lg:col-start-1 lg:row-span-2 lg:row-start-5">
+          <Buttons />
+        </div>
+        <div className="lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1">
+          <About />
+        </div>
+        <div className="lg:col-span-2 lg:col-start-4 lg:row-span-2 lg:row-start-1">
+          <Skills />
+        </div>
+        <div className="lg:col-span-4 lg:col-start-2 lg:row-span-2 lg:row-start-3">
+          <Experience />
+        </div>
+        <div className="lg:col-span-4 lg:row-span-2 lg:row-start-5">
+          <Education />
+        </div>
+        <div className="lg:col-span-5 lg:row-start-7">
+          <Footer />
+        </div>
+      </div>
     </>
   );
 }

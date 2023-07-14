f8ee65a54b25ba5753eceb23a3c32763a29fde14
karolhas <karolekhas@gmail.com>
2023-07-14 18:40:57 +0200

    Changed About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,16 +1,20 @@
 function About() {
   return (
     <>
-      <section id="about" className="m-4 p-6">
-        <h1 className="pb-4 pl-4 text-5xl font-bold">Karol Has</h1>
-        <p className="px-4 text-lg">
-          Ambitious resourceful Junior Front-End Developer who creates websites
-          with passion and dedication. Currently working as a soldier and
-          spending every moment on creating new projects and improving as a
-          programmer. I set goals and achieve them - my current one is to get a
-          job as a software developer.
-        </p>
-      </section>
+      <div className="flex w-full flex-col">
+        <div>
+          <h1 className="text-5xl font-bold">Karol Has</h1>
+        </div>
+        <div>
+          <p className="text-lg">
+            Ambitious resourceful Junior Front-End Developer who creates
+            websites with passion and dedication. Currently working as a soldier
+            and spending every moment on creating new projects and improving as
+            a programmer. I set goals and achieve them - my current one is to
+            get a job as a software developer.
+          </p>
+        </div>
+      </div>
     </>
   );
 }

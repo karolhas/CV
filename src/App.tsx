d58dc31e2ea4220b42ce77ed817032954b533493
karolhas <karolekhas@gmail.com>
2023-07-14 20:59:57 +0200

    Added grid to layout

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,32 @@ import Skills from "./components/Skills";
 
 function App() {
   return (
-    <>
-      <div className="grid grid-cols-5 grid-rows-4 gap-4">
-        <div className="row-span-4">
-          <Photo />
-          <Profile />
-          <Language />
-        </div>
-        <div className="col-span-2">
-          <About />
-        </div>
-        <div className="col-span-2 col-start-4">
-          <Buttons />
-        </div>
-        <div className="col-span-4 col-start-2 row-span-2 row-start-2">
-          <Experience />
-          <Education />
-          <Skills />
-        </div>
-        <div className="col-span-4 col-start-2 row-start-4"></div>
+    <div className="grid max-h-screen max-w-full grid-cols-12 grid-rows-5 gap-12 bg-slate-200">
+      <div className="col-span-3 col-start-1 row-span-2 row-start-1">
+        <Photo />
       </div>
-    </>
+      <div className="col-span-3 col-start-1 row-start-3">
+        <Profile />
+      </div>
+      <div className="col-span-3 col-start-1 row-start-4">
+        <Language />
+      </div>
+      <div className="col-span-5 col-start-4 row-span-2 row-start-1">
+        <About />
+      </div>
+      <div className="col-span-4 col-start-9 row-span-2 row-start-1">
+        <Buttons />
+      </div>
+      <div className="col-span-9 col-start-4 row-start-3">
+        <Experience />
+      </div>
+      <div className="col-span-9 col-start-4 row-start-4">
+        <Education />
+      </div>
+      <div className="col-span-9 col-start-4 row-start-5">
+        <Skills />
+      </div>
+    </div>
   );
 }
 

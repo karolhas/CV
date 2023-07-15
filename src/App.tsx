edc40b4bde7850c36ee7f278df6075a6f825a326
karolhas <karolekhas@gmail.com>
2023-07-15 21:48:16 +0200

    Delete max height and width, changed grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import Photo from "./components/Photo";
 import Profile from "./components/Profile";
-import Buttons from "./components/Buttons";
+import Language from "./components/Language";
 import About from "./components/About";
+import Buttons from "./components/Buttons";
 import Experience from "./components/Experience";
 import Education from "./components/Education";
-import Language from "./components/Language";
 import Skills from "./components/Skills";
 
 function App() {
   return (
-    <div className="grid max-h-screen max-w-full grid-cols-12 grid-rows-5 gap-12 bg-slate-200">
+    <div className="grid-cols-24 grid grid-rows-5 gap-12 bg-slate-100">
       <div className="col-span-3 col-start-1 row-span-2 row-start-1">
         <Photo />
       </div>

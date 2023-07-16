cf2a2e652de02307ebe1c8c8bffc73cfad32c420
karolhas <karolekhas@gmail.com>
2023-07-16 11:54:40 +0200

    Changed grid values

--- src/App.tsx
+++ src/App.tsx
@@ -9,30 +9,30 @@ import Skills from "./components/Skills";
 
 function App() {
   return (
-    <div className="grid min-h-screen grid-cols-12 grid-rows-5 gap-1 bg-slate-200 opacity-50">
-      <div className="col-span-3 col-start-1 row-span-2 row-start-1 bg-slate-400">
-        {/* <Photo /> */} 1
+    <div className="grid-rows-8 grid min-h-screen grid-cols-12 content-center gap-8 p-6">
+      <div className="col-span-3 col-start-1 row-span-3 row-start-1">
+        <Photo />
       </div>
-      <div className="col-span-3 col-start-1 row-start-3 bg-slate-400">
-        {/* <Profile /> */} 2
+      <div className="col-span-3 col-start-1 row-start-4">
+        <Profile />
       </div>
-      <div className="col-span-3 col-start-1 row-start-4 bg-slate-400">
-        {/* <Language /> */} 3
+      <div className="col-span-3 col-start-1 row-start-5">
+        <Language />
       </div>
-      <div className="col-span-5 col-start-4 row-span-2 row-start-1 bg-slate-400">
-        {/* <About /> */} 4
+      <div className="col-span-5 col-start-4 row-span-2 row-start-1 flex items-center">
+        <About />
       </div>
-      <div className="col-span-4 col-start-9 row-span-2 row-start-1 bg-slate-400">
-        {/* <Buttons /> */} 5
+      <div className="col-span-4 col-start-9 row-span-2 row-start-1 flex items-end">
+        <Buttons />
       </div>
-      <div className="col-span-9 col-start-4 row-start-3 bg-slate-400">
-        {/* <Experience /> */} 6
+      <div className="col-span-9 col-start-4 row-start-3">
+        <Experience />
       </div>
-      <div className="col-span-9 col-start-4 row-start-4 bg-slate-400">
-        {/* <Education /> */} 7
+      <div className="col-span-9 col-start-4 row-start-4">
+        <Education />
       </div>
-      <div className="col-span-9 col-start-4 row-start-5 bg-slate-400">
-        {/* <Skills /> */} 8
+      <div className="col-span-9 col-start-4 row-start-5">
+        <Skills />
       </div>
     </div>
   );

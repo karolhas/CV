21af8dfb0d6efbf2768cfe449c98088d72ba5696
karolhas <karolekhas@gmail.com>
2023-07-30 10:28:44 +0200

    fix responsive

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Skills from "./components/Skills";
 
 function App() {
   return (
-    <div className="lg:grid-rows-8 p-8 lg:grid lg:min-h-screen lg:grid-cols-12 lg:content-center lg:gap-8 lg:p-6">
+    <div className="lg:grid-rows-8 p-8 lg:grid lg:min-h-screen lg:grid-cols-12 lg:content-center lg:gap-x-8 lg:gap-y-16 lg:p-6">
       <div className="mb-12 lg:col-span-3 lg:col-start-1 lg:row-span-3 lg:row-start-1 lg:mb-0">
         <Photo />
       </div>
@@ -23,16 +23,16 @@ function App() {
       <div className="mb-12 lg:col-span-5 lg:col-start-4 lg:row-span-2 lg:row-start-1 lg:mb-0 lg:flex lg:items-center">
         <About />
       </div>
-      <div className="mb-12 lg:col-span-4 lg:col-start-9 lg:row-span-2 lg:row-start-1 lg:mb-0 lg:flex lg:items-end lg:pr-2">
+      <div className="mb-12 lg:col-span-4 lg:col-start-9 lg:row-span-2 lg:row-start-1 lg:mb-0 lg:flex lg:items-end lg:pr-4">
         <Buttons />
       </div>
-      <div className="mb-12 lg:col-span-9 lg:col-start-4 lg:row-start-3 lg:mb-0 lg:pr-2">
+      <div className="mb-12 lg:col-span-9 lg:col-start-4 lg:row-start-3 lg:mb-0 lg:pr-4">
         <Experience />
       </div>
-      <div className="mb-12 lg:col-span-9 lg:col-start-4 lg:row-start-4 lg:mb-0 lg:flex lg:flex-col lg:justify-center lg:pr-2">
+      <div className="mb-12 lg:col-span-9 lg:col-start-4 lg:row-start-4 lg:mb-0 lg:flex lg:flex-col lg:justify-center lg:pr-4">
         <Education />
       </div>
-      <div className="lg:col-span-9 lg:row-start-5 lg:pr-2">
+      <div className="lg:col-span-9 lg:row-start-5 lg:pr-4">
         <Skills />
       </div>
     </div>
